0e0a615f4102823edf2552bfb32a982968b5c761
geepair <48669492+geepair@users.noreply.github.com>
2023-09-26 02:34:11 +0800

    Update next.config.js
    add cors domain

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ module.exports = withBundleAnalyzer({
     domains: [
       'gravatar.com',
       'www.notion.so',
+      'amazonaws.com',
       'avatars.githubusercontent.com',
       'images.unsplash.com',
       'source.unsplash.com',
